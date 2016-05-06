9804d03afb3cdc0c477fb6c112685ec4ba899403
JenkaBY <evgen.kuzmich@gmail.com>
2016-05-06 18:05:01 +0300

    made befor page 330

--- workout/src/main/java/com/bts/workout/MainActivity.java
+++ workout/src/main/java/com/bts/workout/MainActivity.java
@@ -3,14 +3,17 @@ package com.bts.workout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorkoutDetailFragment frag = (WorkoutDetailFragment)
-                getFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setWorkoutId(1);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+
     }
 }
