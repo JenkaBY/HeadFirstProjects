b12d24988b5b8cbf1902efaa0e34bf321db21097
JenkaBY <evgen.kuzmich@gmail.com>
2016-06-15 19:08:02 +0300

    p 538

--- starbuzz/src/main/java/com/bts/starbuzz/DrinkActivity.java
+++ starbuzz/src/main/java/com/bts/starbuzz/DrinkActivity.java
@@ -1,28 +1,54 @@
 package com.bts.starbuzz;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
     public final static String EXTRA_DRINKO = "drinkNO";
+    private static final String DB_NAME = "DRINK";
+    private static final String NAME = "NAME";
+    private static final String DESCRIPTION = "DESCRIPTION";
+    private static final String IMAGE_RESOURCE_ID = "IMAGE_RESOURCE_ID";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
         int drinkNo =(Integer) getIntent().getExtras().getInt(EXTRA_DRINKO);
-        Drink drink = Drink.drinks[drinkNo];
+        try{
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query(DB_NAME,new String[]{NAME,DESCRIPTION,IMAGE_RESOURCE_ID},"_id = ?",
+                    new String[]{Integer.toString(drinkNo)},null,null,null);
+            if (cursor.moveToFirst()){
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
 
-        ImageView photo = (ImageView) findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getDescription());
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
 
-        TextView name = (TextView)findViewById(R.id.name);
-        name.setText(drink.getName());
+                TextView name = (TextView)findViewById(R.id.name);
+                name.setText(nameText);
 
-        TextView description = (TextView) findViewById(R.id.description);
-        description.setText(drink.getDescription());
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText(descriptionText);
+            }
+            cursor.close();
+            db.close();
+        }
+        catch (SQLiteException e){
+            Toast toast = Toast.makeText(this,"Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
